7888103924366dffc2567324e94621d2841956a5
Alejandro Diaz <alejandro.diaz@axians.es>
2022-08-29 11:57:59 +0200

    ADD exception handling

--- back/database.py
+++ back/database.py
@@ -1,7 +1,10 @@
+from sqlite3 import OperationalError
+from urllib.error import HTTPError
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:mysql12345@localhost:3306/alexandria")
-
-meta = MetaData()
-
-conn = engine.connect()
+try:
+    engine = create_engine("mysql+pymysql://root:mysql12345@localhost:3306/alexandria")
+    meta = MetaData()
+    conn = engine.connect()
+except OperationalError as err:
+    print(err)
