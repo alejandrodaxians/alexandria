db56d281977858d768b3c2ff91d256b7726d4856
Alejandro Diaz <alejandro.diaz@axians.es>
2022-08-29 12:28:11 +0200

    ADD port for front

--- back/main.py
+++ back/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost"],
+    allow_origins=["http://localhost:8080"], #the front-end will run on this port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
