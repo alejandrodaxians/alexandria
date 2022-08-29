42d3ba0e60eb928a5274a236d55698d0c1bf4e00
Alejandro Diaz <alejandro.diaz@axians.es>
2022-08-29 09:26:32 +0200

    ADD wrapper

--- back/exceptions.py
+++ back/exceptions.py
@@ -1,12 +1,25 @@
-class BookInfoException(Exception):
-    ...
+from fastapi import Request, FastAPI
+from fastapi.responses import JSONResponse
 
-class BookInfoNotFoundError(BookInfoException):
-    def __init__(self):
-        self.status_code = 404
-        self.detail = "Book Info Not Found"
+class BookNotFoundException(Exception):
+    def __init__(self, name: str):
+        self.keyword = name
 
-class BookInfoAlreadyExistsError(BookInfoException):
-    def __init__(self):
-        self.status_code = 409
-        self.detail = "Car Info Already Exists"
+class IdNotFoundException(Exception):
+    def __init__(self, id: int):
+        self.id = id
+
+def exception_handler_wrapper(app: FastAPI):
+    @app.exception_handler(BookNotFoundException)
+    async def book_exception_handler(request: Request, exc: BookNotFoundException):
+        return JSONResponse(
+            status_code=404,
+            content={"message": f"No coincidences found with {exc.keyword}"},
+        )
+
+    @app.exception_handler(IdNotFoundException)
+    async def id_exception_handler(request: Request, exc: IdNotFoundException):
+        return JSONResponse(
+            status_code=404,
+            content={"message": f"No book with id {exc.id} found"},
+        )
