16285568b60d976cce9a184d1ff4dcf3bc118170
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-05 16:37:47 +0200

    UPDATE table declaration syntax

--- back/models/book.py
+++ back/models/book.py
@@ -1,17 +1,18 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table
+
 from back.database.db_connection import DatabaseConnection
 
 db = DatabaseConnection()
 Base = db.declare_base()
 
 class Book(Base):
-    __tablename__ = 'books'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    author = Column(String)
-    genre = Column(String)
-    release_year = Column(Integer)
+    __table__ = Table('books', Base.metadata,
+        Column('id', Integer, primary_key=True),
+        Column('title', String(255)),
+        Column('author', String(255)),
+        Column('genre', String(255)),
+        Column('release_year', Integer)
+    )
 
     
 
