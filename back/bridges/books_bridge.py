e7e76419abeef01317a87edfe9d417b761445dc9
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-05 11:46:01 +0200

    ADD delete & update methods

--- back/bridges/books_bridge.py
+++ back/bridges/books_bridge.py
@@ -1,7 +1,9 @@
-from turtle import title
 from typing import List, Dict
-from back.database.database import conn
+from back.database.db_connection import DatabaseConnection
 from back.models.book import Book
+
+db_conn = DatabaseConnection()
+conn = db_conn.establish_connection()
 
 
 def get_all_books() -> List[Dict]:
@@ -53,13 +55,12 @@ def create_book(book: Book) -> Dict:
 
     Parameters:
 
-    **book: Book**: An object used to accesed the parameters needed to create the book.
+    **book**: An object used to accesed the parameters needed to create the book.
     These are: title(str), author(str), genre(str), release_year(int).
 
     Returns:
         Dict with the new book's information.
     """
-    book = Book()
     query = Book.__tablename__.insert().values(
         title=book.title,
         author=book.author,
@@ -68,3 +69,44 @@ def create_book(book: Book) -> Dict:
     )
     conn.execute(query)
     return book
+
+
+def delete_book(id: int) -> Dict:
+    """
+    **Delete a book by id**
+
+    Parameters:
+
+    **id**: The primary key of the book to be deleted.
+
+    Returns:
+        A Dict containing a message telling the user the book has been succesfully deleted.
+    """
+    result = conn.execute(Book.__tablename__.select().where(Book.__tablename__.c.id == id)).first()
+    book_title = result[0][1]
+    conn.execute(Book.__tablename__.delete().where(Book.__tablename__.c.id == id))
+    return {"message": "Book with title: " + book_title + ", deleted"}
+
+
+def update_book(id: int, book: Book) -> Dict:
+    """
+    **Update a book by id**
+
+    Parameters:
+
+    **id**: The id of the book to be updated.
+    **book**: An object used to accesed the parameters needed to update the book.
+    These are: title(str), author(str), genre(str), release_year(int).
+
+    Returns:
+        A Dict containing a message telling the user the book has been succesfully updated.
+    """
+    exists = conn.execute(Book.__tablename__.select().where(Book.__tablename__.c.id == id)).first()
+    query = Book.__tablename__.update().where(Book.__tablename__.c.id == id).values(
+            title=book.title if book.title != None else exists.title,
+            author=book.author if book.author != None else exists.author,
+            genre=book.genre if book.genre != None else exists.genre,
+            release_year=book.release_year if book.release_year != None else exists.release_year
+        )
+    conn.execute(query)
+    return {"message": "Book with id: " + id + ", updated succesfully."}
