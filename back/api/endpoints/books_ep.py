056c49f1d3dd6603e484b176d5d91a90efd302ee
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-06 09:48:10 +0200

    ADD returning values

--- back/api/endpoints/books_ep.py
+++ back/api/endpoints/books_ep.py
@@ -1,26 +1,28 @@
 from back.api.exceptions import ServerError, BookNotFoundError
-from back.models.book import Book
+from back.database.schemas import Book
 from back.bridges import books_bridge
 from back.config import loggers
 
 from fastapi import APIRouter, Query, status
 from pydantic import Required
+from typing import Dict, List
 
-api_logger = loggers.get_logger('main')
 router = APIRouter()
+api_logger = loggers.get_logger('main')
 
 
 @router.get("/book/",
     summary="Get all books", 
     description="Output all books currently in the database, ordered by id.",
     tags=["books"],
-    response_model=Book,
+    response_model=List[Book],
     status_code=status.HTTP_200_OK,
     )
-async def get_all_books():
+def get_all_books():
     try:
-        books_bridge.get_all_books()
+        query_result = books_bridge.get_all_books()
         api_logger.info("Collection succesfully retrieved.")
+        return query_result
     except ServerError as srve:        
         api_logger.error('{} -> There was an error retrieving the collection, with the following message: {}'\
                         .format(srve.status_code, srve.message))
@@ -36,8 +38,9 @@ async def get_all_books():
     )
 async def get_book_by_id(id: int = Query(default=Required)):
     try:
-        books_bridge.get_book_by_id(id)
+        query_result = books_bridge.get_book_by_id(id)
         api_logger.info(f"Book with id: '{id}' succesfully found.")
+        return query_result
     except ServerError as srve:
         api_logger.error('{} -> There was an error retrieving the book with id {}, with the following message: {}'\
                         .format(srve.status_code, id, srve.message))
@@ -46,18 +49,19 @@ async def get_book_by_id(id: int = Query(default=Required)):
                         .format(bnfe.status_code, id, bnfe.message))
         
     
-@router.get("/book/{title}/", 
+@router.get("/book/title/{title}/", 
     summary="Get books by title",
     description="""Input a keyword and output all books that contain that keyword in their title.
                 If there are no books with that keyword, an error is returned.""",
     tags=["books"],
-    response_model=Book,
+    response_model=List[Book],
     status_code=status.HTTP_200_OK,
     )
-async def get_book_by_title(title: str = Query(default=Required, min_length=3)):
+async def get_book_by_title(title: str = Query(default=Required)):
     try:
-        books_bridge.get_book_by_title(title)
+        query_result = books_bridge.get_book_by_title(title)
         api_logger.info(f"Book with title keyword coincidence: '{title}' succesfully found.")
+        return query_result
     except ServerError as srve:
         api_logger.error('{} -> There was an error retrieving the book with search keyword {}, with the following message: {}'\
                         .format(srve.status_code, title, srve.message))
@@ -73,10 +77,11 @@ async def get_book_by_title(title: str = Query(default=Required, min_length=3)):
     response_model=Book,
     status_code=status.HTTP_201_CREATED,
     )
-async def create_book(book: Book = Query(default=Required)):
+async def create_book(book: Book):
     try:
-        books_bridge.create_book(book)
+        create_result = books_bridge.create_book(book)
         api_logger.info(f"Book created succesfully, with id: '{book.id}'.")
+        return create_result
     except ServerError as srve:
         api_logger.error('{} -> There was an error creating he book, with the following message: {}'\
                         .format(srve.status_code, srve.message))
@@ -87,30 +92,31 @@ async def create_book(book: Book = Query(default=Required)):
     description="""Input an id and delete the corresponding book. 
                 If the id is not on the database, an exception will be raised.""",
     tags=["books"],
-    response_model=Book,
     status_code=status.HTTP_200_OK,
     )
 async def delete_book(id: int):
     try:
-        books_bridge.delete_book(id)
+        delete_result = books_bridge.delete_book(id)
         api_logger.info(f"Book with id: '{id}' deleted.")
+        return delete_result
     except ServerError as srve:
         api_logger.error('{} -> There was an error deleting he book, with the following message: {}'\
                         .format(srve.status_code, srve.message))
 
 
-@router.put("/books/{id}/",
+@router.put("/book/{id}/",
     summary="Update a book",
     description="""Input an id and update the book with that id. 
                 If the id is not on the database, an exception will be raised.""",
     tags=["books"],
-    response_model=Book,
+    response_model=Dict,
     status_code=status.HTTP_200_OK,
     )
 async def update_book(id: int, book: Book):
     try:
-        books_bridge.update_book(id, book)
+        update_result = books_bridge.update_book(id, book)
         api_logger.info(f"Book with id: '{id}' succesfully updated.")
+        return update_result
     except ServerError as srve:
         api_logger.error('{} -> There was an error updating the book with search keyword {}, with the following message: {}'\
                         .format(srve.status_code, id, srve.message))
