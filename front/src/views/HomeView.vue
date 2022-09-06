362e7e336fa786da29049cb97daf1bf4498f9a2b
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-06 13:00:35 +0200

    REMOVE hardcoded sidebar component

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -4,37 +4,15 @@
       <h1>ALEXANDRIA</h1>
       <h1>VIRTUAL LIBRARY</h1>
     </div>
-    <div v-if="show" class="sidebar">
-      <h3 class="sidebar-item">MENU</h3>
-      <br>
-      <a href="/collection">Collection</a>
-      <br><br>
-      <a href="/search_book">Search books</a>
-      <br><br>
-      <a href="/new_book">Create a book</a>
-      <br><br>
-      <a href="/delete_book">Delete a book</a>
-      <br><br>
-      <a href="/update_book">Update a book</a>
-    </div>
-    <div class="button">
-      <button @click="show = !show" class="toggle"><menu-icon /></button>
-    </div>
   </body>
 </template>
 
 <script>
-import MenuIcon from 'vue-material-design-icons/Menu.vue';
-
 export default {
   name: 'HomeView',
-  components: {
-    MenuIcon,
-  },
+  components: {},
   data() {
-    return {
-      show: false,
-    }
+    return {}
   },
   methods: {
   }
@@ -43,5 +21,5 @@ export default {
 
 <style>
 @import '@/assets/styles/home_style.scss';
-@import '@/assets/styles/sidebar.scss';
+@import 'vue-material-design-icons/styles.css';
 </style>
