5800d2ca095aca7e5c3098098d2601f24595705e
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-06 11:13:30 +0200

    UNIFY book search into one component

---
+++ front/src/components/GetBook.vue
@@ -0,0 +1,59 @@
+<template>
+  <body>
+
+    <div class="searchID">
+      <input type="text" v-model="id">
+      <button class="detail" @click="searchById()">
+        SEARCH by ID
+      </button>
+      <p>{{ id_msg }}</p>
+    </div>
+
+    <div class="searchTitle">
+      <input type="text" v-model="keyword">
+      <button class="detail" @click="searchByTitle()">
+        SEARCH by TITLE
+      </button>
+      <p>{{ title_msg }}</p>
+    </div>
+    
+  </body>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      id_msg: '',
+      title_msg: '',
+      id: '',
+      keyword: '',
+    };
+  },
+  methods: {
+    searchById() {
+      axios.get('http://localhost:8000/book/' + this.id)
+        .then((res) => {
+          this.id_msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    },
+    searchByTitle() {
+      axios.get('http://localhost:8000/book/title/' + this.keyword)
+        .then((res) => {
+          this.title_msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+      })
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
