514a9a3fe4b8451cb2688026616eeac2ea89080a
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-06 13:02:14 +0200

    RENDER sidebar in every page

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
     <router-view/>
+    <side-bar/>
   </div>
 </template>
+
+<script>
+import SideBar from '/src/components/SideBar.vue'
+
+export default {
+  components: {
+    SideBar,
+  }
+}
+</script>
 
 <style scoped>
 </style>
