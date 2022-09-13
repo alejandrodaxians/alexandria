33f229a761d76ee17f5e5d4ecb96218f418719ec
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-13 13:44:56 +0200

    FIX mixup between schemas & models

--- back/database/schemas.py
+++ back/database/schemas.py
@@ -1,10 +1,16 @@
-from typing import Optional
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Table
+
+from back.database.db_connection import DatabaseConnection
 
 
-class Book(BaseModel):
-    id: Optional[int] = None
-    title: str
-    author: str
-    genre: str
-    release_year: int
+db = DatabaseConnection()
+Base = db.base
+
+
+class Book(Base):
+    __table__ = Table('books', Base.metadata,
+                      Column('id', Integer, primary_key=True),
+                      Column('title', String(255)),
+                      Column('author', String(255)),
+                      Column('genre', String(255)),
+                      Column('release_year', Integer))
